4def175b8db0df669307fab0fb69dd40ce5148b0
ragaldino <rafaela.galdino@mercadolivre.com>
2022-02-10 12:24:12 -0300

    Criação de enum para novos TrackingEvents (estudo de bugs duplicados)

---
+++ MercadoPagoSDK/MercadoPagoSDK/UI/PXPaymentsInfoGeneralEvents.swift
@@ -0,0 +1,36 @@
+//
+//  PXPaymentsInfoGeneralEvents.swift
+//  MercadoPagoSDKV4
+//
+
+import Foundation
+
+enum PXPaymentsInfoGeneralEvents: TrackingEvents {
+    case infoGeneral_Follow
+    case infoGeneral_Follow_Payments([String: Any])
+    case infoGeneral_Follow_Confirm_Payments([String: Any])
+
+    var name: String {
+        switch self {
+        case .infoGeneral_Follow: return "/px_checkout/follow"
+        case .infoGeneral_Follow_Payments: return "/px_checkout/follow/payments"
+        case .infoGeneral_Follow_Confirm_Payments: return "/px_checkout/follow/confirm_payments"
+        }
+    }
+
+    var properties: [String: Any] {
+        switch self {
+        case .infoGeneral_Follow: return [:]
+        case .infoGeneral_Follow_Payments(let properties), .infoGeneral_Follow_Confirm_Payments(let properties): return properties
+        }
+    }
+
+    var needsExternalData: Bool {
+        switch self {
+        case .infoGeneral_Follow:
+            return true
+        case .infoGeneral_Follow_Payments, .infoGeneral_Follow_Confirm_Payments:
+            return false
+        }
+    }
+}
