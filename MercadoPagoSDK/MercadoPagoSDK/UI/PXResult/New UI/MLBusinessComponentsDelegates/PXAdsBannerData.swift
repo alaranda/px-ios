140a49b27d19d4014d7c4bc391094a6d391fb882
alaranda <alexis.aranda@mercadolibre.cl>
2022-03-01 10:18:39 -0300

    feature: banner interface development

--- MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/MLBusinessComponentsDelegates/PXAdsBannerData.swift
+++ MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/MLBusinessComponentsDelegates/PXAdsBannerData.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import MLBusinessComponents
+
+class PXAdsBannerData: NSObject, MLBusinessAdsBannerData {
+    let banner: PXAdsBanner
+
+    init(banner: PXAdsBanner) {
+        self.banner = banner
+    }
+
+    func getImageUrl() -> String {
+        return banner.content.imgUrl
+    }
+
+    func getDeepLink() -> String {
+        return banner.content.deepLink
+    }
+
+    func getClickUrl() -> String {
+        return banner.content.destinationUrl
+    }
+
+    func getPrintUrl() -> String {
+        return banner.content.printUrl
+    }
+}
